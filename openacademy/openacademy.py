93a5c8c41a6a328ae1a04a03e975a66eb4a83cd7
Max Mumford (maxmumford@gmail.com) <>
2012-10-22 15:11:52 +0200

    calculate percentage filled on attendees / seats

--- openacademy/openacademy.py
+++ openacademy/openacademy.py
@@ -11,24 +11,31 @@ class Course(osv.Model):
         'name': fields.char('Name', size = 128, required = True),
         'description': fields.text('Description'),
 
-        "session_ids" : fields.one2many("openacademy.session", "course_id", String="Session"),
-        "responsible_id" : fields.many2one("res.users", String = "Responsible"),
+        "session_ids" : fields.one2many("openacademy.session", "course_id", string="Session"),
+        "responsible_id" : fields.many2one("res.users", string = "Responsible"),
     }
 
 class Session(osv.Model):
 	_name = 'openacademy.session'
+
+	def _calculate_percentage_filled(self, cr, uid, ids, name, arg, context = None):
+		res = {}
+		for session in self.browse(cr, uid, ids, context):
+			res[session.id] = 100 * float(len(session.attendee_ids)) / float(session.seats) if session.seats else 0
+		return res
 
 	_columns = {
 		'name': fields.char('Name', size = 128, required = True),
 		"startdate" : fields.datetime("StartDate"),
 		"duration" : fields.float("Duration", digits = (5,1), help = "The duration of the session in hours"),
 		"seats" : fields.integer("Seats"),
+		"percentage_filled" : fields.function(_calculate_percentage_filled, type="integer", string="Percentage Filled"),
 
-		"instructor_id" : fields.many2one("res.partner", String="Instructor", 
+		"instructor_id" : fields.many2one("res.partner", string="Instructor", 
 			domain = ['|', ("is_instructor", '=', True), ("category_id", "child_of", "Teacher")]
 		),
-		"course_id" : fields.many2one("openacademy.course", required = True, String = "Related Course"),
-		"attendee_ids" : fields.one2many("openacademy.attendee", "session_id", String = "Attendees"),
+		"course_id" : fields.many2one("openacademy.course", required = True, string = "Related Course"),
+		"attendee_ids" : fields.one2many("openacademy.attendee", "session_id", string = "Attendees"),
 		"partner_id" : fields.many2one("res.partner", required = True)
 	}
 	
@@ -38,6 +45,6 @@ class Attendee(osv.Model):
 	_columns = {
         'name': fields.char('Name', size = 128, required = False),
 
-        "partner_id" : fields.many2one("res.partner", String="Partner"),
+        "partner_id" : fields.many2one("res.partner", string="Partner"),
     	"session_id" : fields.many2one("openacademy.session", required = True)
     }
